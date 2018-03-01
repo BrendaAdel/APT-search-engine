929df2a5be31dccc297f6e47a80761259890faf7
BrendaAdel <brendaadel96@gmail.com>
2018-03-01 10:06:06 +0200

    Add files via upload

---
+++ APT_Project.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apt_project;
+
+/**
+ *
+ * @author Brenda
+ */
+public class APT_Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Thread crawler = new Thread(new Crawler());
+        crawler.start();
+        Thread indexer = new Thread(new Indexer());
+        indexer.start();
+        
+    }
+    
+}
---
+++ Indexer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apt_project;
+
+/**
+ *
+ * @author Brenda
+ */
+public class Indexer implements Runnable {
+
+    @Override
+    public void run() {
+       System.out.println("hello from indexer");
+    }
+    
+}
